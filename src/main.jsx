4c1c0bb6f2fed6375ea90559384ea846ac2ba0d6
Sumitra Paul <sumitra.cse.just2020@gmail.com>
2024-01-31 23:06:55 +0600

    movie details page show

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,7 @@ import {
 } from "react-router-dom";
 import Root from './Root/Root';
 import Home from './Pages/Home/Home';
+import ShowDetails from './Pages/ShowDetails/ShowDetails';
 
 const router = createBrowserRouter([
   {
@@ -16,6 +17,10 @@ const router = createBrowserRouter([
       {
         path: "/",
         element: <Home></Home>
+      },
+      {
+        path: "/details/:id",
+        element: <ShowDetails/>
       },
     ],
   },
