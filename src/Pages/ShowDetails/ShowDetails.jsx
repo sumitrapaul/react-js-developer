b358b8dd5c1a8668af7b351c93e6ee9eba31a0ef
Sumitra Paul <sumitra.cse.just2020@gmail.com>
2024-02-01 00:25:42 +0600

    booking data save in the localstorage

--- src/Pages/ShowDetails/ShowDetails.jsx
+++ src/Pages/ShowDetails/ShowDetails.jsx
@@ -1,31 +1,91 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import Form from 'react-bootstrap/Form';
+import Modal from 'react-bootstrap/Modal';
 
 const ShowDetails = () => {
-    const { id } = useParams()
-    const [showDetails, setShowDetails] = useState(null)
-   
-    useEffect(() => {
-        fetch(`https://api.tvmaze.com/shows/${id}`)
-          .then((res) => res.json())
-          .then((data) => {
-            console.log(data)
-            setShowDetails(data)
-          });
-      }, [id]);
-    return (
-        <div>
-           
-           {showDetails ? (
-            <>
-            <p>{showDetails?.name}</p>
-            </>
-           ) : (
-            <p>Loading...</p>
-           )}
-        </div>
-    );
+  const { id } = useParams();
+  const [showDetails, setShowDetails] = useState(null);
+  const [show, setShow] = useState(false);
+  useEffect(() => {
+    fetch(`https://api.tvmaze.com/shows/${id}`)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setShowDetails(data);
+      });
+  }, [id]);
+
+  const customButtonStyle = {
+    background: "linear-gradient(30deg, #0891b2, #a21caf)",
+  };
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  const handleSubmit = (e) =>{
+    e.preventDefault()
+    const bookingData ={
+        name: showDetails?.name || "",
+    summary: showDetails?.summary || ""
+    }
+    localStorage.setItem('bookingData',JSON.stringify(bookingData))
+    handleClose()
+  }
+
+  return (
+    <div>
+      <Card>
+        <Card.Header>{showDetails?.name}</Card.Header>
+        <Card.Body>
+          <Card.Title>Type: {showDetails?.type}</Card.Title>
+          <Card.Text>
+            {showDetails?.summary}
+          </Card.Text>
+          <Card.Body>
+        <Card.Link href={showDetails?.officialSite}>OfficialSite</Card.Link>
+        <Card.Link href={showDetails?._links?.self?.href}>Self Link</Card.Link>
+      </Card.Body>
+          <Button onClick={handleShow} style={customButtonStyle}>Booking Movie Ticket</Button>
+        </Card.Body>
+      </Card>
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Booking Details</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form onSubmit={handleSubmit}>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+              <Form.Label>Name</Form.Label>
+              <Form.Control
+                type="name"
+                placeholder="Enter your name"
+                autoFocus
+              />
+            </Form.Group>
+            <Form.Group
+              className="mb-3"
+              controlId="exampleForm.ControlTextarea1"
+            >
+              <Form.Label>Summary</Form.Label>
+              <Form.Control as="textarea" rows={3} />
+            </Form.Group>
+            <Button style={customButtonStyle} type="submit">
+            Submit
+          </Button>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button style={customButtonStyle} onClick={handleClose}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </div>
+  );
 };
 
 export default ShowDetails;
