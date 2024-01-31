30e1fb945acf48c16ae4bbf4f54a8c430dd94639
Sumitra Paul <sumitra.cse.just2020@gmail.com>
2024-01-31 23:41:01 +0600

    details page with data show

--- src/Pages/ShowDetails/ShowDetails.jsx
+++ src/Pages/ShowDetails/ShowDetails.jsx
@@ -1,9 +1,29 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 
 const ShowDetails = () => {
+    const { id } = useParams()
+    const [showDetails, setShowDetails] = useState(null)
+   
+    useEffect(() => {
+        fetch(`https://api.tvmaze.com/shows/${id}`)
+          .then((res) => res.json())
+          .then((data) => {
+            console.log(data)
+            setShowDetails(data)
+          });
+      }, [id]);
     return (
         <div>
-            
+           
+           {showDetails ? (
+            <>
+            <p>{showDetails?.name}</p>
+            </>
+           ) : (
+            <p>Loading...</p>
+           )}
         </div>
     );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -20,7 +20,8 @@ const router = createBrowserRouter([
       },
       {
         path: "/details/:id",
-        element: <ShowDetails/>
+        element: <ShowDetails/>,
+        loader:({params}) => fetch(`https://api.tvmaze.com/shows?${params.id}`)
       },
     ],
   },
